6b72360640ea5710c0e9470f954df7fac8a8f94d
Eddie Villanueva <edvillan15@gmail.com>
2024-03-23 04:57:12 +0000

    modified:   app/layout.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,37 @@
-import ClientOnly from '@/app/components/ClientOnly';
-import ToasterProvider from '@/app/components/providers/ToasterProvider';
-import '@radix-ui/themes/styles.css';
-import { Metadata } from 'next';
-import { Nunito } from 'next/font/google';
-import './globals.css';
+import ClientOnly from "@/app/components/ClientOnly";
+import ToasterProvider from "@/app/components/providers/ToasterProvider";
+import "@radix-ui/themes/styles.css";
+import { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
 export const metadata: Metadata = {
-	title: 'Meetlily Dashboard',
-	description: 'Simplify Data Management'
+  title: "Meetlily Dashboard",
+  description: "Simplify Data Management",
 };
 
 const font = Nunito({
-	subsets: ['latin']
+  subsets: ["latin"],
 });
 
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body
-				className={`
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`
                 ${font.className}
               bg-gray-100 dark:bg-gray-800
             `}
-			>
-				<ClientOnly>
-					<ToasterProvider />
+      >
+        <ClientOnly>
+          <ToasterProvider />
 
-					{children}
-				</ClientOnly>
-			</body>
-		</html>
-	);
+          {children}
+        </ClientOnly>
+      </body>
+    </html>
+  );
 }
